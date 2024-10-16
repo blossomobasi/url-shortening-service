77782482a9d3b2113ced9332b3f781679156e389
Obasi Chizitere <blossomobasi2@gmail.com>
2024-10-16 06:41:02 +0100

    Update sub-routes

--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -2,15 +2,13 @@ import express from "express";
 import {
     createShortURL,
     deleteShortURL,
-    getAllUrls,
     getOriginalURL,
     statsOnShortURL,
     updateShortURL,
 } from "../controllers/url.controller";
 const router = express.Router();
 
-router.route("/create").post(createShortURL);
-router.route("/").get(getAllUrls).patch(updateShortURL).delete(deleteShortURL);
-router.route("/original-url").post(getOriginalURL);
-router.route("/stats").get(statsOnShortURL);
+router.route("/").post(createShortURL);
+router.route("/:shortUrl").get(getOriginalURL).patch(updateShortURL).delete(deleteShortURL);
+router.route("/:shortUrl/stats").get(statsOnShortURL);
 export default router;
