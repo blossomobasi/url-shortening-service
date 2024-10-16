9f2746c2e26447475473aa0096615ec5968460c7
Obasi Chizitere <blossomobasi2@gmail.com>
2024-10-16 06:40:33 +0100

    configure local env

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: ".env.local" });
 
 const connectDB = async (): Promise<void> => {
     try {
