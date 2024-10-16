41ef3c9d1f719218393536c600820a0682f8c598
Obasi Chizitere <blossomobasi2@gmail.com>
2024-10-16 05:59:44 +0100

    Implement database connection

---
+++ src/db/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async (): Promise<void> => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string);
+        console.log("<::: Connected to MongoDB :::>");
+    } catch (error) {
+        console.log("<::: Error Connecting to MongoDB :::>", error);
+    }
+};
+
+export default connectDB;
