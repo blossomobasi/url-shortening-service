fd43cd78a14bc75b8ad9e7263532acd8123c500e
Obasi Chizitere <blossomobasi2@gmail.com>
2024-10-16 06:00:13 +0100

    Create url default routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,19 @@
+import { Express, Request, Response } from "express";
+import urlRoutes from "./urlRoutes";
+
+export default function routes(app: Express) {
+    app.get("/", (req: Request, res: Response) => {
+        res.status(200).json({
+            status: "success",
+            message: "URL Shortening Service",
+        });
+    });
+    app.use("/api/urls", urlRoutes);
+
+    app.all("*", (req: Request, res: Response) => {
+        res.status(404).json({
+            status: "fail",
+            message: `Can't find ${req.originalUrl} on this server!`,
+        });
+    });
+}
